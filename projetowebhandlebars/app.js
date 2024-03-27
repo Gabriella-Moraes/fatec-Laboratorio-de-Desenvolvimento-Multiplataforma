ef7b6305f1cfb7a5906ddab8a595ee313c4433f2
Gabriella-Moraes <gabriellamoraes58@gmail.com>
2024-03-27 13:47:29 -0300

    aula 27/03 e atividade de responsividade

--- projetowebhandlebars/app.js
+++ projetowebhandlebars/app.js
@@ -1,22 +1,51 @@
-const express = require("express")
-const handlebars = require("express-handlebars").engine
-const app = express()
+const express = require("express");
+const handlebars = require("express-handlebars").engine;
+const app = express();
+const bodyParser = require("body-parser");
+const post = require("./models/post");
 
-app.engine("handlebars", handlebars({defaultLayout: "main"}))
-app.set("view engine", "handlebars")
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.listen(8081, function(){
-    console.log(`Servidor ativo!`)
-})
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get("/", function(req, res){
+app.listen(8081, function () {
+  console.log(`Servidor ativo!`);
+});
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.post("/cadastrar", function (req, res) {
+    //res.send("pagina cadastrar")
+  post
+    .create({
+      nome: req.body.nome,
+      telefone: req.body.telefone,
+      origem: req.body.origem,
+      data_contato: req.body.data_contato,
+      observacao: req.body.observacao,
+    })
+    .then(function () {
+      console.log("Agendamento cadastrado com sucesso!");
+    })
+    .catch(function (erro) {
+      console.log("Erro: Agendamento não cadastrado!" + erro);
+    });
+
     res.render("index")
-})
+});
 
-app.get("/consultar", function(req, res){
-    res.render("consultar")
-})
+app.get("/consultar", function (req, res) {
+  post.findAll().then(function(post){
+    res.render("consultar", {post: post})
+  }).catch(function(erro){
+    console.log("Erro: Nenhum agendamento encontrado!" + erro)
+  })
+});
 
-app.get("/atualizar", function(req, res){
-    res.render("atualizar")
-})
+app.get("/atualizar", function (req, res) {
+  res.render("atualizar");
+});
