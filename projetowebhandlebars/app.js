8a7c6bdf68c60a79a893777b6c60d616f4852506
Gabriella-Moraes <gabriellamoraes58@gmail.com>
2024-04-06 15:53:00 -0300

    aula 03/04 + atividade

--- projetowebhandlebars/app.js
+++ projetowebhandlebars/app.js
@@ -46,6 +46,42 @@ app.get("/consultar", function (req, res) {
   })
 });
 
-app.get("/atualizar", function (req, res) {
-  res.render("atualizar");
-});
+
+app.get("/editar/:id", function (req, res) {
+  post.findAll({ where: { "id": req.params.id } }).then(function (post) {
+      console.log(post)
+      res.render("editar", {post: post})
+  }).catch(function (erro) {
+      console.log("Erro: Agendamento não encontrado!" + erro)
+  })
+})
+
+app.post("/atualizar", function (req, res) {
+  post.update({
+    nome: req.body.nome,
+    telefone: req.body.telefone,
+    origem: req.body.origem,
+    data_contato: req.body.data_contato,
+    observacao: req.body.observacao
+  }, { where: { "id": req.body.id } }).then(function () {
+    console.log("Agendamento atualizado com sucesso!")
+    }).catch(function (erro) {
+      console.log("Erro: nenhum agendamento encontrado! " + erro)
+    })
+    
+    post.findAll().then(function(post){
+      res.render("consultar", {post: post})
+    }).catch(function(erro){
+      console.log("Erro: Nenhum agendamento encontrado!" + erro)
+    })
+})
+
+app.get("/excluir/:id", function (req, res) {
+  post.destroy({ where: { "id": req.params.id } }).then(function () {
+    res.render("index")
+    console.log("Agendamento excluído com sucesso!")
+  }).catch(function (erro) {
+    console.log("Erro: Agendamento não excluído!" + erro)
+  })
+})
+
